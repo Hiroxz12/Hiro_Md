95ed5b2b251973f2d9e501edab5aa72e0317452a
Hiroxz12 <110282352+Hiroxz12@users.noreply.github.com>
2022-07-30 16:38:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ let chalk = require('chalk')
 owner = [
   ['6285732267014'],
   ['6258732267014'],
-  ['62895330379186', 'Owner', true]
+  ['6285732267014', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 mods = [] // Want some help?
